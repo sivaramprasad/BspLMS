8261f7ce6781e74985a5d82afe85fe02bbb9f345
Sivak <sivak@avetti.ca>
2025-04-10 16:31:42 +0530

    Test

--- next.config.js
+++ next.config.js
@@ -1,22 +1,13 @@
-/** @type {import('next').NextConfig} */
-
-// const withTM = require('next-transpile-modules')(['@babel/preset-react']);
-//   '@fullcalendar/common',
-//   '@fullcalendar/common',
-//   '@fullcalendar/daygrid',
-//   '@fullcalendar/interaction',
-//   '@fullcalendar/react',
-
 const nextConfig = {
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  output: 'export',
+  basePath: '',
+  assetPrefix: '',
   images: {
     domains: [
       'images.unsplash.com',
       'i.ibb.co',
       'scontent.fotp8-1.fna.fbcdn.net',
     ],
-    // Make ENV
     unoptimized: true,
   },
 };
--- src/app/admin/changepassword/page.tsx
+++ src/app/admin/changepassword/page.tsx
@@ -138,7 +138,7 @@ console.log(result.status);
 
   return (
     <>
-      <div className="mt-[10vh] w-full max-w-full flex-col items-center md:pl-4 lg:pl-0 xl:max-w-[420px]">
+      <div className="mt-[10vh] w-full max-w-full flex-col items-center md:pl-4 lg:pl-0 xl:max-w-[420px] loginColor">
         <h3 className="mb-2.5 text-4xl font-bold text-navy-700 dark:text-white">
           Change Password
         </h3>
